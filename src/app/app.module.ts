0170303a72f902e4f15312cc6fd120bb554f57ef
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2018-06-27 18:29:09 +0530

    Added Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+   { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  imports: [BrowserModule,RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
