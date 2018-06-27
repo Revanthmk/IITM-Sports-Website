4b49d0c20618fd801ee489f7ab75cfa95b753db6
shivtavker <tavkershiv@gmail.com>
2018-06-27 19:38:48 +0530

    Fixed Navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  showNavbar: boolean;
+
+  constructor() {
+    console.log(window.outerWidth);
+    if (window.outerWidth < 770) this.showNavbar = false;
+    else this.showNavbar = true;
+  }
+
+  toggleNavbar() {
+    this.showNavbar = !this.showNavbar;
+  }
+}
