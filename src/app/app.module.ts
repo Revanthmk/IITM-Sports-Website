98dbe9a9f78d7d64452af7ce94739ed3c14f5312
shivtavker <tavkershiv@gmail.com>
2018-07-06 16:36:04 +0530

    Designed Custom Calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
+// ----------- 3rd Party Modules ------------------ //
+
+// ----------- Custom Components ------------------ //
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { CalendarDateComponent } from './components/calendar-date/calendar-date.component';
+
+// ----------- Custom Pages ---------------------- //
+
+import { CalendarPageComponent } from './pages/calendar-page/calendar-page.component';
+
+// ----------- App Routes ------------------------ //
 
 const appRoutes: Routes = [
-   { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'calendar', component: CalendarPageComponent },
 ];
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent],
-  imports: [BrowserModule,RouterModule.forRoot(appRoutes)],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CalendarDateComponent,
+    CalendarPageComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/calendar-date/calendar-date.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-date',
+  templateUrl: './calendar-date.component.html',
+  styleUrls: ['./calendar-date.component.css']
+})
+export class CalendarDateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
